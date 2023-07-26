1a69b642ccd3671437ea9ecc7394a6300b5f2125
seungmidev <seungmilee.dev@gmail.com>
2023-07-26 13:39:26 +0100

    Add react-query

---
+++ basic/src/AppQuery.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import MainProducts from './components/MainProducts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+
+const queryClient = new QueryClient();
+
+export default function AppQuery() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <MainProducts />
+      <ReactQueryDevtools initialIsOpen={true} />
+    </QueryClientProvider>
+  );
+}
